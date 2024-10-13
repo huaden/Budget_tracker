90514b93454bdfb69f8409c7a58b2d188e2ec0d0
huaden <161155310+huaden@users.noreply.github.com>
2024-10-13 20:45:51 +0200

    intial project strucutre in fully set up

---
+++ flaskBackend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask Backend"
+
+if __name__ == '__main__':
+    app.run(debug=True)
