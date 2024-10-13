985a729a5107dbdf1efa62013b4e4b6b86b0b155
huaden <161155310+huaden@users.noreply.github.com>
2024-10-13 20:57:45 +0200

    Database is made now

--- flaskBackend/app.py
+++ flaskBackend/app.py
@@ -8,9 +8,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+class Purchases(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f'<Purchase made on {self.date} for the amount of {self.amount}>'
+    
+
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def home():
     return "Welcome to the Flask Backend"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
